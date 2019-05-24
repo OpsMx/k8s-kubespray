770a212ad691d207cc07e8cedfb1f58df6b4e594
mgseelan <37366247+mgseelan@users.noreply.github.com>
2019-05-24 20:29:47 +0530

    updated

--- opsmx-k8s/nfs-auto.sh
+++ opsmx-k8s/nfs-auto.sh
@@ -3,24 +3,20 @@
 #### Organization : OpsMX
 #### Author : M Gnana Seelan
 
-node_list=("x.x.x.x" "x.x.x.x" "x.x.x.x")
-user_names=("username" "username" "username")
+node_list=("10.170.0.11" "10.170.0.8" "10.170.0.9")
 
 # This is required to be executed for each minion node.
 function nfs_client() {
   local -n nodelist=$1
-  local -n userlist=$2
-  #printf '1: %q\n' "${nodelist[@]}"
-  #printf '2: %q\n' "${userlist[@]}"
   for ((i=0;i<${#nodelist[@]};++i))
   do
     echo ""
-    echo "************* Connecting to ${nodelist[i]} with ${userlist[i]} *************"
+    echo "************* Connecting to ${nodelist[i]}  *************"
     # Download nfs-common on each target node
     echo "Started installing nfs-common..."
-    ssh ${userlist[i]}@${nodelist[i]} "sudo apt-get update && sudo apt-get install -y nfs-common"
+    ssh -A ${nodelist[i]} "sudo apt-get update && sudo apt-get install -y nfs-common"
     echo "Started verifying nfs connection between client and server..."
-    ssh ${userlist[i]}@${nodelist[i]} "showmount -e $nfs_server_ip"
+    ssh -A ${nodelist[i]} "showmount -e $nfs_server_ip"
   done
 }
 
@@ -85,30 +81,30 @@ fi
 echo ".......... Starting to install nfs server ......"
 sudo apt-get update && sudo apt-get install -y nfs-kernel-server
 echo ".......... Creating a folder for nfs share ......"
-echo " Enter NFS Folder name which need to be created ( example : /home/nfsshare ): "
-read -r nfsfolder
-echo " Enter the private ip range with which the nfs share folder will be exposed ( for example 10.0.0.0/18): " 
-read -r nfs_share_iprange	
+printf "\n"
+read -p " Enter NFS Folder name which need to be created ( example : /home/nfsshare ): " nfsfolder
+echo ".. NFS folder is creating..."
+read -p  " Enter the private ip range with which the nfs share folder will be exposed ( for example 10.0.0.0/18): " nfs_share_iprange
+echo " updating ip range of client to be exposed "
 sudo mkdir -p $nfsfolder
 echo ".......... Changing ownership and permission to folder for nfs share ......"
 sudo chown nobody:nogroup $nfsfolder	
 sudo chmod 777 $nfsfolder
 echo ".......... Updating the exports file with nfs share folder details ......"
-sudo su
+# sudo su
 sudo echo '$nfsfolder $nfs_share_iprange(rw,sync,no_subtree_check,no_root_squash)' >> /etc/exports
-exit
+# exit
 echo ".......... Checking the NFS configuration details ......"
 sudo exportfs -a	
 sudo exportfs
 echo ".......... NFS Server configutaion sucessfully completed ......"
 
 echo "Started installing nfs-common on each nfs-client ..."
-nfs_client node_list user_names
+nfs_client node_list
 echo "Installed nfs_client successfully"
 
 echo ".......... NFS client provisioning setup in the k8s-cluster ......"
-echo " Enter NFS server ip ( example : 10.165.0.56 ): "
-read -r nfs_server_ip
+read -p  " Enter NFS server ip ( example : 10.165.0.56 ): " nfs_server_ip
+echo " Installing nfs client provisioner in k8s cluster"
 helm install stable/nfs-client-provisioner --name nfs --set nfs.server=$nfs_server_ip --set nfs.path=$nfsfolder
 echo ".......... NFS Server configutaion with nfs-client provisioning is sucessfully completed in k8s-cluster......"
-
