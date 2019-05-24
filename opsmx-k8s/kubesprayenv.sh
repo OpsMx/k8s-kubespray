492c4ea229aeaa1e78ead8075859b4c827173715
mgseelan <37366247+mgseelan@users.noreply.github.com>
2019-05-24 17:58:05 +0530

    Updated

--- opsmx-k8s/kubesprayenv.sh
+++ opsmx-k8s/kubesprayenv.sh
@@ -6,6 +6,25 @@
 
 #### Ansible Installation: In current setup we are using HAProxy system as our launch machine
 
+node_list=("x.x.x.x" "x.x.x.x" "x.x.x.x")
+user_names=("username" "username" "username")
+
+# Installing python is required to test ansible ping on each node .
+function install_prerequiste() {
+  local -n nodelist=$1
+  local -n userlist=$2
+  #printf '1: %q\n' "${nodelist[@]}"
+  #printf '2: %q\n' "${userlist[@]}"
+  for ((i=0;i<${#nodelist[@]};++i))
+  do
+    echo ""
+    echo "************* Connecting to ${nodelist[i]} with ${userlist[i]} *************"
+    # Download nfs-common on each target node
+    echo "Started installing python..."
+    ssh ${userlist[i]}@${nodelist[i]} "sudo apt-get update && sudo apt-get install -y python"
+  done
+}
+
 ## Step to Create Virtual environment and Install Ansible in launch machine
 echo " .... Setting up virtual environment for installing ansible playbook .........."
 
@@ -18,6 +37,9 @@ sudo apt-get update && sudo apt-get install -y ansible python-netaddr
 echo " .... Installing python-pip and virtualenv on the machine to initiate kubespray .........."
 sudo apt-get update && sudo apt-get install -y python-pip  virtualenv
 
+echo "Started installing pre-requiste on each node ..."
+install_prerequiste node_list user_names
+echo "Installed pre-requiste successfully"
 
 
 
